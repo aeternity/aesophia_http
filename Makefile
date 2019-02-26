cb73940fe9f028e4ba8dce367362880685e46b33
Hans Svensson <hans.svensson@quviq.com>
2019-02-26 12:17:06 +0100

    Initial addition, swagger + HTTP routing

---
+++ Makefile
@@ -0,0 +1,54 @@
+SWAGGER_CODEGEN_CLI_V = 2.3.1
+SWAGGER_CODEGEN_CLI = swagger/swagger-codegen-cli-$(SWAGGER_CODEGEN_CLI_V).jar
+SWAGGER_CODEGEN = java -jar $(SWAGGER_CODEGEN_CLI)
+SWAGGER_ENDPOINTS_SPEC = apps/aesophia_http/src/endpoints.erl
+
+SWTEMP := $(shell mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
+HTTP_APP := apps/aesophia_http
+MAKE := make
+
+REBAR ?= rebar3
+
+all:
+	@($(REBAR) compile)
+
+swagger: config/swagger.yaml $(SWAGGER_CODEGEN_CLI) $(SWAGGER_ENDPOINTS_SPEC)
+	@$(SWAGGER_CODEGEN) generate -i $< -l erlang-server -o $(SWTEMP)
+	@echo "Swagger tempdir: $(SWTEMP)"
+	@( mkdir -p $(HTTP_APP)/priv && cp $(SWTEMP)/priv/swagger.json $(HTTP_APP)/priv/; )
+	@$(MAKE) updateswagger
+	@rm -fr $(SWTEMP)
+
+$(SWAGGER_CODEGEN_CLI):
+	curl -fsS --create-dirs -o $@ http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/$(SWAGGER_CODEGEN_CLI_V)/swagger-codegen-cli-$(SWAGGER_CODEGEN_CLI_V).jar
+
+$(SWAGGER_ENDPOINTS_SPEC):
+	$(REBAR) swagger_endpoints
+
+SWAGGER_UI_GIT = https://github.com/swagger-api/swagger-ui.git
+SWAGGER_UI_GIT_DIR = $(HTTP_APP)/priv/swagger-ui
+SWAGGER_DOCS_DIR = $(HTTP_APP)/priv/swagger-docs
+
+swagger-docs: |$(SWAGGER_UI_GIT_DIR)/.git
+	@mkdir -p $(SWAGGER_DOCS_DIR)
+	@cp -p $(SWAGGER_UI_GIT_DIR)/dist/* $(SWAGGER_DOCS_DIR)
+	@cp -p $(HTTP_APP)/priv/swagger.json $(SWAGGER_DOCS_DIR)
+	@sed -ibkp 's/https:\/\/petstore.swagger.io\/v2\/swagger.json/swagger.json/g' $(SWAGGER_DOCS_DIR)/index.html
+
+updateswagger:
+	@if [ -f $(SWAGGER_DOCS_DIR)/swagger.json ]; then \
+	        cp -p $(HTTP_APP)/priv/swagger.json $(SWAGGER_DOCS_DIR); \
+	fi
+
+$(SWAGGER_UI_GIT_DIR)/.git:
+	@git clone -n --depth 1 $(SWAGGER_UI_GIT) $(SWAGGER_UI_GIT_DIR)
+	( cd $(SWAGGER_UI_GIT_DIR) && git checkout master dist ; )
+
+clean:
+	rm -rf $(SWAGGER_DOCS_DIR)
+	@$(REBAR) clean
+	@-rm $(SWAGGER_ENDPOINTS_SPEC)
+
+distclean:
+	rm -rf $(SWAGGER_DOCS_DIR) $(SWAGGER_UI_GIT_DIR)
+
--- apps/aesophia_http/src/aesophia_http_handler.erl
+++ apps/aesophia_http/src/aesophia_http_handler.erl
@@ -1,65 +1,96 @@
 -module(aesophia_http_handler).
 
 -export([init/2,
-	 handle/2,content_types_provided/2,
+	 handle_request_json/2,content_types_provided/2,
 	 allowed_methods/2,content_types_accepted/2
 	]).
 
 -compile(export_all).
 
-init(Req0, Opts) ->
-    %% Method = cowboy_req:method(Req0),
-    %% io:format("Method: ~p\nReq: ~p\nOpts: ~p\n", [Method,Req0,Opts]),
-    {cowboy_rest,Req0,Opts}.
+-record(state, { spec :: jsx:json_text()
+               , validator :: jesse_state:state()
+               , operation_id :: atom() }).
+
+init(Req, OperationId) ->
+    JsonSpec = aehttp_api_validate:json_spec(),
+    Validator = aehttp_api_validate:validator(JsonSpec),
+    State = #state{ spec = JsonSpec,
+                    validator = Validator,
+                    operation_id = OperationId },
+    {cowboy_rest, Req, State}.
 
 allowed_methods(Req, State) ->
     Methods = [<<"GET">>,<<"POST">>],
     {Methods,Req,State}.
 
 content_types_accepted(Req, State) ->
-    {[
-      {{<<"application">>,<<"json">>,'*'}, handle},
-      {{<<"text">>,<<"plain">>,'*'}, handle}
-     ], Req, State}.
+    {[{{<<"application">>, <<"json">>, '*'}, handle_request_json}], Req, State}.
 
 content_types_provided(Req, State) ->
-    {[
-      {<<"application/json">>,handle},
-      {<<"test/plain">>,handle}
-     ], Req, State}.
+    {[{<<"application/json">>, handle_request_json}], Req, State}.
 
-handle(Req, Opts) ->
-    Method = cowboy_req:method(Req),
-    case Method of
-	<<"POST">> ->
-	    handle_post(Req, Opts);
-	_ ->
-	    {stop,cowboy_req:reply(405, Req),Opts}
+handle_request_json(Req0, State = #state{ validator = Validator,
+                                         spec = Spec,
+                                         operation_id = OperationId }) ->
+    Method = cowboy_req:method(Req0),
+    try aesophia_http_api_validate:request(OperationId, Method, Req0, Validator) of
+        {ok, Params, Req1} ->
+            Context = #{ spec => Spec },
+            {Code, Headers, Body} = handle_request(OperationId, Params, Context),
+
+            _ = aesophia_http_api_validate:response(OperationId, Method, Code, Body, Validator),
+
+            Req = cowboy_req:reply(Code, to_headers(Headers), jsx:encode(Body), Req1),
+            {stop, Req, State};
+        {error, Reason, Req1} ->
+            Body = jsx:encode(to_error(Reason)),
+            Req = cowboy_req:reply(400, #{}, Body, Req1),
+            {stop, Req, State}
+    catch error:_Error ->
+            Body = jsx:encode(to_error({validation_error, <<>>, <<>>})),
+            {stop, cowboy_req:reply(400, #{}, Body, Req0), State}
     end.
 
-handle_post(Req0, Opts) ->
-    {ok,Body,Req1} = cowboy_req:read_body(Req0, #{}),
-    Path = cowboy_req:path(Req1),
-    %% io:format("Body: ~p\n", [Body]),
-    case Path of
-	<<"/contracts/code/aci">> ->
-	    #{<<"code">> := Code, <<"options">> := Options} =
-		jsx:decode(Body, [return_maps]),
-	    Headers = #{<<"content-type">> => <<"application/json">>},
-	    ACI = aci_encode(Code, Options),
-	    Req2 = cowboy_req:reply(200, Headers, ACI, Req1),
-	    {stop,Req2,Opts};
-	<<"/contracts/code/compile">> ->
-	    #{<<"code">> := Code, <<"options">> := Options} =
-		jsx:decode(Body, [return_maps]),
-	    Headers = #{<<"content-type">> => <<"application/json">>},
-	    Ret = compile_code(Code, Options),
-	    Req2 = cowboy_req:reply(200, Headers, Ret, Req1),
-	    {stop,Req2,Opts};
-	_ ->
-	    Req2 = cowboy_req:reply(400, #{}, <<"Bad command.">>, Req1),
-	    {stop,Req2,Opts}
-    end.
+handle_request('CompileContract', Req, _Context) ->
+    case Req of
+        #{'Contract' :=
+              #{ <<"code">> := Code
+               , <<"options">> := Options }} ->
+            case compile_contract(Code, Options) of
+                 {ok, ByteCode} ->
+                     {200, [], #{bytecode => aehttp_api_encoder:encode(contract_bytearray, ByteCode)}};
+                 {error, ErrorMsg} ->
+                     {403, [], #{reason => ErrorMsg}}
+             end;
+        _ -> {403, [], #{reason => <<"Bad request">>}}
+    end;
+
+handle_request('Api', _Req, #{ spec := Spec }) ->
+    {200, [], Spec}.
+
+%% handle_post(Req0, Opts) ->
+%%     {ok,Body,Req1} = cowboy_req:read_body(Req0, #{}),
+%%     Path = cowboy_req:path(Req1),
+%%     %% io:format("Body: ~p\n", [Body]),
+%%     case Path of
+%% 	<<"/contracts/code/aci">> ->
+%% 	    #{<<"code">> := Code, <<"options">> := Options} =
+%% 		jsx:decode(Body, [return_maps]),
+%% 	    Headers = #{<<"content-type">> => <<"application/json">>},
+%% 	    ACI = aci_encode(Code, Options),
+%% 	    Req2 = cowboy_req:reply(200, Headers, ACI, Req1),
+%% 	    {stop,Req2,Opts};
+%% 	<<"/contracts/code/compile">> ->
+%% 	    #{<<"code">> := Code, <<"options">> := Options} =
+%% 		jsx:decode(Body, [return_maps]),
+%% 	    Headers = #{<<"content-type">> => <<"application/json">>},
+%% 	    Ret = compile_code(Code, Options),
+%% 	    Req2 = cowboy_req:reply(200, Headers, Ret, Req1),
+%% 	    {stop,Req2,Opts};
+%% 	_ ->
+%% 	    Req2 = cowboy_req:reply(400, #{}, <<"Bad command.">>, Req1),
+%% 	    {stop,Req2,Opts}
+%%     end.
 
 %% aci_encode(Contract, Options) -> ACI.
 
@@ -69,26 +100,19 @@ aci_encode(Contract, _Options) ->
     <<"{\"encoded-aci\":",Enc/binary,",",
       "\"decoded-aci\":\"",Dec/binary,"\"}">>.
 
-%% compile_code(Contract, Options) -> ByteCode.
-%%  Compile a contract code, then serialize it and encode it as a
-%%  contract bytearray. The code for this has been plucked from
-%%  aect_sophia:compile/2 and aehttp_dispatch_int:handle_request_/3
-%%  for 'CompileContract'. Yes, it's a hack at the moment.
-
-compile_code(Contract, Options) ->
-    %% aect_sophia:compile/2
-    Map = aeso_compiler:from_string(binary_to_list(Contract), []),
-    Ser = serialize(Map),
-    %% io:format("Ser: ~p\n", [Ser]),
-    %% aehttp_dispatch_int:handle_request_('CompileContract', ...)
-    ByteCode = aehttp_api_encoder:encode(contract_bytearray, Ser),
-    <<"{\"bytecode\":\"",ByteCode/binary,"\"}">>.
+compile_contract(Contract, _Options) ->
+    case aeso_compiler:from_string(binary_to_list(Contract), []) of
+        {ok, Map} ->
+            {ok, serialize(Map)};
+        Err = {error, _} ->
+            Err
+    end.
 
 %% From aect_sophia.erl for compile/2
 
 -define(SOPHIA_CONTRACT_VSN, 1).
 
-serialize(#{byte_code := ByteCode, type_info := TypeInfo, 
+serialize(#{byte_code := ByteCode, type_info := TypeInfo,
             contract_source := ContractString, compiler_version := _Version}) ->
     ContractBin = list_to_binary(ContractString),
     Fields = [ {source_hash, aec_hash:hash(sophia_source_code, ContractBin)}
@@ -105,3 +129,13 @@ serialization_template(?SOPHIA_CONTRACT_VSN) ->
     [ {source_hash, binary}
     , {type_info, [{binary, binary, binary, binary}]} %% {type hash, name, arg type, out type}
     , {byte_code, binary}].
+
+to_headers(Headers) when is_list(Headers) ->
+    maps:from_list(Headers);
+to_headers(Headers) ->
+    Headers.
+
+to_error({Reason, Name, Info}) ->
+    #{ reason => Reason,
+       parameter => Name,
+       info => Info }.
