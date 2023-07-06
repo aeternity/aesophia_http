656116baf5b4f905a5ae38c1d70deba50077cc52
Hans Svensson <hanssv@gmail.com>
2023-07-06 10:12:41 +0200

    Set idle_timeout to 10 minutes

--- apps/aesophia_http/src/aesophia_http_app.erl
+++ apps/aesophia_http/src/aesophia_http_app.erl
@@ -18,14 +18,12 @@ start(_StartType, _StartArgs) ->
     Paths = get_paths(),
     {ok,Port} = application:get_env(port),      %Get the port
     logger:info("Port: ~p", [Port]),
-    Dispatch = cowboy_router:compile([
-				      {'_',Paths}
-				     ]),
-    {ok,_} =  cowboy:start_clear(http, [{port,Port}],
-				 #{env => #{dispatch => Dispatch},
-                   middlewares => [cowboy_router,
-                                   aesophia_cors_middleware,
-                                   cowboy_handler]}),
+    Dispatch = cowboy_router:compile([{'_', Paths}]),
+    Middlewares = [cowboy_router, aesophia_cors_middleware, cowboy_handler],
+    {ok, _} = cowboy:start_clear(http, [{port, Port}],
+				 #{env          => #{dispatch => Dispatch},
+                                   middlewares  => Middlewares,
+                                   idle_timeout => 10 * 60000}),
     aesophia_http_sup:start_link().
 
 %%--------------------------------------------------------------------
